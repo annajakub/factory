f6ec8b4ee94fa8080079f69980782a91e45c7aa1
Anna <annajakub@wp.pl>
2017-08-06 11:17:35 +0200

    + the absolute value

---
+++ src/main/java/TheAbsotuteValue.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class TheAbsotuteValue {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner (System.in);
+        int number;
+        int result;
+
+        System.out.println("Wpisz liczbę");
+        number = myScanner.nextInt();
+
+        result = getAbsolute(number);
+
+        System.out.println("Wynik:" + result);
+    }
+
+   public static int getAbsolute(int number) {
+       int result;
+       if (number >0) {
+         result =  number;
+       }
+    else {
+        result = number * -1;
+   }
+   return result;
+}
